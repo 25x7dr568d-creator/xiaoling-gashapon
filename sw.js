39ce4b0f970c81fcf5aeb2466ccc33f500cb4691
25x7dr568d-creator <25x7dr568d-creator@users.noreply.github.com>
2026-02-28 18:27:28 +0800

    Initial deploy: 小伶魔法世界扭蛋機

---
+++ sw.js
@@ -0,0 +1,36 @@
+var CACHE_NAME = 'gashapon-v7';
+
+self.addEventListener('install', function(event) {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(function(cache) {
+      return cache.addAll(['./', './index.html']);
+    })
+  );
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', function(event) {
+  event.waitUntil(
+    caches.keys().then(function(names) {
+      return Promise.all(
+        names.filter(function(n) { return n !== CACHE_NAME; })
+          .map(function(n) { return caches.delete(n); })
+      );
+    })
+  );
+  self.clients.claim();
+});
+
+self.addEventListener('fetch', function(event) {
+  event.respondWith(
+    caches.match(event.request).then(function(cached) {
+      return cached || fetch(event.request).then(function(res) {
+        if (res && res.status === 200) {
+          var clone = res.clone();
+          caches.open(CACHE_NAME).then(function(c) { c.put(event.request, clone); });
+        }
+        return res;
+      });
+    })
+  );
+});
